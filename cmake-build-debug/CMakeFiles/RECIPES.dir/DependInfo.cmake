7099faf6b1ec566ec156bed3739b64464c5ee226
loohan <looper.han@nokia-sbell.com>
2019-02-14 17:57:09 +0800

    add files

---
+++ cmake-build-debug/CMakeFiles/RECIPES.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/loohan/CLionProjects/recipes/main.cpp" "C:/Users/loohan/CLionProjects/recipes/cmake-build-debug/CMakeFiles/RECIPES.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/N-5CG719422G-Data/loohan/Desktop/nokia/OpenSource/boost_1_64_0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RECIPES.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RECIPES.dir/main.cpp.obj"
+  "RECIPES.pdb"
+  "RECIPES.exe"
+  "RECIPES.exe.manifest"
+  "libRECIPES.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RECIPES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
